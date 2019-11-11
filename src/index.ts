7619493f853bb796b0c0161bedd162e4e2585899
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2019-11-11 22:11:44 +0100

    Removed imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import QRCodeStyling from "./core/QRCodeStyling";
 import dotTypes from "./constants/dotTypes";
-import errorCorrectionLevels from "./constants/errorCorrectionLevels";
+//import errorCorrectionLevels from "./constants/errorCorrectionLevels";
 import errorCorrectionPercents from "./constants/errorCorrectionPercents";
-import modes from "./constants/modes";
-import qrTypes from "./constants/qrTypes";
+// import modes from "./constants/modes";
+// import qrTypes from "./constants/qrTypes";
 
 export default QRCodeStyling;
-export { dotTypes, errorCorrectionLevels, errorCorrectionPercents, modes, qrTypes };
+export { dotTypes, /*errorCorrectionLevels,*/ errorCorrectionPercents /*modes,*/ /*qrTypes*/ };
