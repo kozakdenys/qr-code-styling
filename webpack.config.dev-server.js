01edcebdb45fb90b4b35af9b8671aea8d5e8fe96
Denys Kozak <denys.kozak@idalko.com>
2020-12-19 14:08:34 +0200

    Update modules

--- webpack.config.dev-server.js
+++ webpack.config.dev-server.js
@@ -1,17 +1,17 @@
-const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.config.common.js');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
-  devServer: {},
+  devServer: {
+    injectClient: false //workaround for bug https://github.com/webpack/webpack-dev-server/issues/2484
+  },
   devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       inject: 'head'
-    }),
-    new webpack.HotModuleReplacementPlugin(),
+    })
   ]
 });
