a672c275e4e675488c01ffd4bb4895829c3284d0
NickStull <stull.nicholas@gmail.com>
2023-11-10 12:28:20 -0600

    Update Git repository with code changes

--- src/figures/cornerDot/canvas/QRCornerDot.ts
+++ src/figures/cornerDot/canvas/QRCornerDot.ts
@@ -19,6 +19,9 @@ export default class QRCornerDot {
       case cornerDotTypes.square:
         drawFunction = this._drawSquare;
         break;
+      // case cornerDotTypes.heart:
+      //   drawFunction = this._drawHeart;
+      //   break;
       case cornerDotTypes.dot:
       default:
         drawFunction = this._drawDot;
@@ -32,10 +35,10 @@ export default class QRCornerDot {
     const cy = y + size / 2;
 
     context.translate(cx, cy);
-    rotation && context.rotate(rotation);
+    // rotation && context.rotate(rotation);
     draw();
     context.closePath();
-    rotation && context.rotate(-rotation);
+    // rotation && context.rotate(-rotation);
     context.translate(-cx, -cy);
   }
 
@@ -61,6 +64,44 @@ export default class QRCornerDot {
     });
   }
 
+  // _basicHeart(args: BasicFigureDrawArgsCanvas): void {
+  //   const { x, y, size, context } = args;
+
+  //   this._rotateFigure({
+  //     ...args,
+  //     draw: () => {
+  //       context.save();
+  //       context.scale(size / 700, size / 700);
+  //       context.moveTo(x / 2, y / 2);
+  //       context.beginPath();
+  //       context.moveTo(480, -120);
+  //       context.lineTo(422, -172);
+  //       context.quadraticCurveTo(321, -263, 255, -329);
+  //       context.quadraticCurveTo(189, -395, 150, -447.5);
+  //       context.quadraticCurveTo(111, -500, 95.5, -544);
+  //       context.quadraticCurveTo(80, -588, 80, -634);
+  //       context.quadraticCurveTo(80, -728, 143, -791);
+  //       context.quadraticCurveTo(206, -854, 300, -854);
+  //       context.quadraticCurveTo(352, -854, 399, -832);
+  //       context.quadraticCurveTo(446, -810, 480, -770);
+  //       context.quadraticCurveTo(514, -810, 561, -832);
+  //       context.quadraticCurveTo(608, -854, 660, -854);
+  //       context.quadraticCurveTo(754, -854, 817, -791);
+  //       context.quadraticCurveTo(880, -728, 880, -634);
+  //       context.quadraticCurveTo(880, -588, 864.5, -544);
+  //       context.quadraticCurveTo(849, -500, 810, -447.5);
+  //       context.quadraticCurveTo(771, -395, 705, -329);
+  //       context.quadraticCurveTo(639, -263, 538, -172);
+  //       context.lineTo(480, -120);
+  //       context.closePath();
+  //       context.fill();
+  //       context.stroke();
+  //       context.restore();
+  //       context.restore();
+  //     }
+  //   });
+  // }
+
   _drawDot({ x, y, size, context, rotation }: DrawArgsCanvas): void {
     this._basicDot({ x, y, size, context, rotation });
   }
@@ -68,4 +109,8 @@ export default class QRCornerDot {
   _drawSquare({ x, y, size, context, rotation }: DrawArgsCanvas): void {
     this._basicSquare({ x, y, size, context, rotation });
   }
+
+  // _drawHeart({ x, y, size, context, rotation }: DrawArgsCanvas): void {
+  //   this._basicHeart({ x, y, size, context, rotation });
+  // }
 }
