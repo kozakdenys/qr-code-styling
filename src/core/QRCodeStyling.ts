088ce336879c8e972a2bdcea9e406fc0e2294108
Patipat C. <patipat.che@mahidol.edu>
2025-01-13 22:22:36 +0700

    feat: fix string to bytes support utf-8 string

--- src/core/QRCodeStyling.ts
+++ src/core/QRCodeStyling.ts
@@ -125,7 +125,7 @@ export default class QRCodeStyling {
     if (!this._options.data) {
       return;
     }
-
+    qrcode.stringToBytes = qrcode.stringToBytesFuncs['UTF-8'];
     this._qr = qrcode(this._options.qrOptions.typeNumber, this._options.qrOptions.errorCorrectionLevel);
     this._qr.addData(this._options.data, this._options.qrOptions.mode || getMode(this._options.data));
     this._qr.make();
